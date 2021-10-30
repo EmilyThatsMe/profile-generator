608abcfb0dde22471771860e9f5d585448a0c4a3
EmilyT <emilython@hotmail.com>
2021-10-30 15:36:23 -0500

    added employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,24 @@
+const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee.js');
 
 test('creates new Employee', () => {
-    const employee = new Employee('name');
+    const employee = new Employee('Bob');
+});
 
-    expect(employee.name).toBe('name');
+test('gets id', () => {
+    const idNumber = 1234;
+    const employee = new Employee('Bob', idNumber);
+    expect(employee.id).toBe(idNumber);
+});
+
+test('gets email', () => {
+    const testEmail = 'bob@gmail.com';
+    const employee = new Employee('Bob', 1, testEmail);
+    expect(employee.email).toBe(testEmail);
+});
+
+test('gets role', () => {
+    const testRole = 'Employee';
+    const employee = new Employee('Bob', 1, 'bob@gmail.com');
+    expect(employee.getRole()).toBe(testRole);
 });
